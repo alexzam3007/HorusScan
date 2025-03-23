75356d6f6628fddc24885411d6fc7b991fdffa39
alexzam3007 <alezambrano300796@gmail.com>
2025-03-23 20:54:31 -0300

    subiendo cambios del main. Versiom 0.0.2

--- cmd/main.go
+++ cmd/main.go
@@ -6,13 +6,19 @@ import (
 )
 
 func main() {
+
+	// ----------------------- Bloque 0, Bienvenida a la app --------------------------------------- //
+
 	// Mostrar mensaje de bienvenida
 	fmt.Println("Bienvenido a HorusScan, la herramienta de diagnóstico y testeo de equipos. ¡Comencemos!")
+
+
+	// ----------------------- Bloque 1, informacion del equipo ---------------------------------- //
 
 	// Mostrar mensaje de informacion del equipo
 	fmt.Println("Informacion del equipo:")
 
-	// Obtener la información del equipo
+	// Obtener informacion del CPU
 	cpu, err := scanner.GetCPUInfo()
 	if err != nil {
 		fmt.Println("Error obteniendo información del CPU:", err)
@@ -20,6 +26,7 @@ func main() {
 		fmt.Printf("CPU: %s\n", cpu)
 	}
 
+	// Obtener informacion de la memoria RAM
 	ram, err := scanner.GetRAMInfo()
 	if err != nil {
 		fmt.Println("Error obteniendo información de la RAM:", err)
@@ -27,6 +34,7 @@ func main() {
 		fmt.Printf("RAM: %.2f GB\n", ram)
 	}
 
+	// Obtener informacion del disco
 	disks, err := scanner.GetDiskInfo()
 	if err != nil {
 		fmt.Println("Error obteniendo información de los discos:", err)
@@ -37,6 +45,7 @@ func main() {
 		}
 	}
 
+	// Obtener informacion de la bateria
 	battery, err := scanner.GetBatteryInfo()
 	if err != nil {
 		fmt.Println("Error obteniendo información de la batería:", err)
@@ -44,7 +53,15 @@ func main() {
 		fmt.Printf("Batería: %s\n", battery)
 	}
 
+	// ----------------------- Bloque 2, Espera por el usuario --------------------------------- //
+
 	// Mostrar mensaje de finalización y esperar entrada del usuario
 	fmt.Println("\nPresiona Enter para empezar el test...")
 	fmt.Scanln() // Espera la entrada del usuario para continuar
+
+
+	// ----------------------- Bloque 3, Test del equipo ---------------------------------- //
+
+
+
 }
